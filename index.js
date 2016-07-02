e2fe623c42b2d63f6f02ed2c8f46973f149b32b6
Jacqueline Peh <jacqueline@craftandcode.com.sg>
2016-07-02 17:09:38 +0800

    added func to send message

--- index.js
+++ index.js
@@ -44,3 +44,26 @@ app.post('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+
+
+
+// FUNCTION TO SEND MESSAGE
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
