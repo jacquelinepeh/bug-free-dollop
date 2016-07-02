648071dea4219d349a72ee8f4c2f19d02110540e
Yishu See <yishuyishu@gmail.com>
2016-07-02 19:50:47 +0800

    integrate messenger

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const token = process.env.FB_PAGE_ACCESS_TOKEN
+const witToken = '2RPYNUGN4FPKR3RFFHJSDYQSYWUGHEWK'
+
+let Wit = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+}
+
+// Setting up our bot
+const wit = new Wit({
+  accessToken: witToken,
+  actions,
+  logger: new log.Logger(log.INFO)
+});
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -21,14 +37,14 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'process.env.FB_VERIFY_TOKEN') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    /*let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
@@ -36,7 +52,29 @@ app.post('/webhook/', function (req, res) {
             let text = event.message.text
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
-    }
+    }*/
+    wit.runActions(
+      sessionId, // the user's current session
+      text, // the user's message
+      sessions[sessionId].context // the user's current session state
+    ).then((context) => {
+      // Our bot did everything it has to do.
+      // Now it's waiting for further messages to proceed.
+      console.log('Waiting for next user messages');
+
+      // Based on the session state, you might want to reset the session.
+      // This depends heavily on the business logic of your bot.
+      // Example:
+      // if (context['done']) {
+      //   delete sessions[sessionId];
+      // }
+
+      // Updating the user's current session state
+      sessions[sessionId].context = context;
+    })
+    .catch((err) => {
+      console.error('Oops! Got an error from Wit: ', err.stack || err);
+    })
     res.sendStatus(200)
 })
 
@@ -67,3 +105,85 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  /*send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },*/
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  getCrude({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      var word = firstEntityValue(entities, 'word');
+      console.log(entities);
+      if (word) {
+        if (word == "shit") {
+          context.scottish = "jobby";
+        } else if (word == "idiot") {
+          context.scottish = "bawbag";
+        } else {
+          context.scottish = "bollocks"
+        }
+      }
+      return resolve(context);
+    });
+  },
+};
